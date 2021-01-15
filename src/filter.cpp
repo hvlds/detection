74b0f36cb7a5967c007f90e235ed7c6d93551d3b
Hernán Valdés <hfvaldesg@gmail.com>
2021-01-15 08:09:58 +0100

    Add config files for apriltags

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Int32.h"
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
@@ -7,19 +8,23 @@
 
 image_transport::Publisher image_pub;
 
-void chatterCallback(const sensor_msgs::ImageConstPtr& img)
+void arduinoCallback(const int& msg) {
+
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& img)
 {
+    // Recieve IR frame from camera in MONO16 (GRAY16 in OpenNI2) format
     cv_bridge::CvImagePtr cv_ptr;
     cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO16);
     cv::Mat img8(480, 640, CV_8UC1);
     cv_ptr->image.convertTo(img8, CV_8UC1);
     cv::imshow("8-bit Mono", img8);
     
+    // Publish the image in new topic (can be processed from rqt_image_view)
     sensor_msgs::ImagePtr msg;
     msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", img8).toImageMsg();
     image_pub.publish(msg);
-
-    //cv::waitKey(1);
 }
 
 int main(int argc, char **argv)
@@ -33,7 +38,9 @@ int main(int argc, char **argv)
     image_transport::ImageTransport it(n);
     image_pub = it.advertise("filtered_camera/image", 1);
 
-    ros::Subscriber sub = n.subscribe(camera_topic, 1000, chatterCallback);
+    ros::Subscriber arduino_sub = n.subscribe("brightness", 10, arduinoCallback);
+
+    ros::Subscriber image_sub = n.subscribe(camera_topic, 10, imageCallback);
     ros::spin();
 
     return 0;
