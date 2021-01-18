f5f3a14895202bc4fb3c8745e9eba442d9ebb00a
Hernán Valdés <hfvaldesg@gmail.com>
2021-01-18 17:27:35 +0100

    Create the node 'analyser' for the analysis of the brightness (arduino and image processing)

--- src/analyser.cpp
+++ src/analyser.cpp
@@ -11,6 +11,13 @@
 #include <iostream>
 
 image_transport::Publisher image_pub;
+int brightness = -1;
+
+void arduinoCallback(const std_msgs::Int32::ConstPtr& msg) {
+    // Assigned value read from the Arduino
+    brightness = msg->data;
+    ROS_INFO("brightness: %i", brightness);
+}
 
 void imageCallback(const sensor_msgs::ImageConstPtr& img) {
     // Recieve IR frame from camera in MONO16 (GRAY16 in OpenNI2) format
@@ -18,26 +25,17 @@ void imageCallback(const sensor_msgs::ImageConstPtr& img) {
     cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO16);
     cv::Mat img8(480, 640, CV_8UC1);
     cv_ptr->image.convertTo(img8, CV_8UC1);
-
-    // Publish the image in new topic (can be processed from rqt_image_view)
-    sensor_msgs::ImagePtr msg;
-    msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", img8).toImageMsg();
-
-    // Publish the new formatted image with same header as the input image
-    msg->header = img->header; 
-    image_pub.publish(msg);
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "filter");
+    ros::init(argc, argv, "analyser");
     ros::NodeHandle n;
     image_transport::ImageTransport it(n);
 
-    // Assign parsed parameters from ./config/detection/general.yaml
-    std::string camera_topic;
-    if(n.getParam("/detection/camera_topic", camera_topic) == false) {
-        ROS_ERROR("Failed to get param '/detection/camera_topic'");
-        camera_topic = "/camera/ir/image_raw";
+    std::string arduino_topic;
+    if(n.getParam("/detection/arduino_topic", arduino_topic) == false) {
+        ROS_ERROR("Failed to get param '/detection/arduino_topic'");
+        arduino_topic = "/brightness";
     }
 
     std::string filtered_image_topic;
@@ -46,7 +44,8 @@ int main(int argc, char** argv) {
         filtered_image_topic = "/camera/ir/image_filtered";
     }
 
-    ros::Subscriber image_sub = n.subscribe(camera_topic, 10, imageCallback);
+    ros::Subscriber filtered_image_sub = n.subscribe(filtered_image_topic, 10, imageCallback);
+    ros::Subscriber arduino_sub = n.subscribe(arduino_topic, 10, arduinoCallback);
 
     image_pub = it.advertise(filtered_image_topic, 1);
 
